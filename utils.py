c6bb12a859bf1b5efa2f7587aa9810ad67744bc8
Ming Ying <yingming18@gmail.com>
2019-07-23 11:21:06 -0400

    Add utils.py

---
+++ utils.py
@@ -0,0 +1,15 @@
+import logging
+import os
+
+LOG_LEVEL = 'DL_LOG_LEVEL'
+
+def get_logger(name):
+    formatter = logging.Formatter('%(asctime)s %(levelname)s: (%(name)s) - %(message)s')
+    # formatter = logging.Formatter('[%(levelname)s] %(module)s - %(message)s')
+    log_level = os.environ.get('DL_LOG_LEVEL', 'INFO')
+    log = logging.getLogger(name)
+    log.setLevel(log_level)
+    std_handler = logging.StreamHandler()
+    std_handler.setFormatter(formatter)
+    log.addHandler(std_handler)
+    return log
