0ec0681c30f5aa7400d2f3d067132efd26246d26
Ming Ying <yingming18@gmail.com>
2019-08-29 17:21:29 -0400

    Added get_uuid_for_node function, to generate v5 UUIDs for given node type and signature.

--- utils.py
+++ utils.py
@@ -1,7 +1,9 @@
 import logging
 import os
+import uuid
 
 LOG_LEVEL = 'DL_LOG_LEVEL'
+ICDC_DOMAIN = 'caninecommons.cancer.gov'
 
 def get_logger(name):
     formatter = logging.Formatter('%(asctime)s %(levelname)s: (%(name)s) - %(message)s')
@@ -13,3 +15,20 @@ def get_logger(name):
     std_handler.setFormatter(formatter)
     log.addHandler(std_handler)
     return log
+
+def get_uuid_for_node(node_type, signature):
+    """Generate V5 UUID for a node
+    Arguments:
+        node_type - a string represents type of a node, e.g. case, study, file etc.
+        signature - a string that can uniquely identify a node within it's type, e.g. case_id, clinical_study_designation etc.
+                    or a long string with all properties and values concat together if no id available
+
+    """
+    log = get_logger('Utils')
+    icdc_base_uuid = uuid.uuid5(uuid.NAMESPACE_URL, ICDC_DOMAIN)
+    # log.debug('Base UUID: {}'.format(icdc_base_uuid))
+    type_uuid = uuid.uuid5(icdc_base_uuid, node_type)
+    # log.debug('Type UUID: {}'.format(type_uuid))
+    node_uuid = uuid.uuid5(type_uuid, signature)
+    log.debug('Node UUID: {}'.format(node_uuid))
+    return node_uuid
