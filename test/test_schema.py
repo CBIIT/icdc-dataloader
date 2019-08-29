1042508b9366b2f109143d746ceb84486edad2de
Ming Ying <yingming18@gmail.com>
2019-08-29 16:55:21 -0400

    Add testing on node count and relationship count to schema testing.

--- test/test_schema.py
+++ test/test_schema.py
@@ -7,7 +7,11 @@ class TestSchema(unittest.TestCase):
     def test_schema_construction(self):
         self.assertRaises(Exception, ICDC_Schema, None)
         self.assertRaises(Exception, ICDC_Schema, ['a', 'b'])
-        self.assertIsInstance(ICDC_Schema(['data/icdc-model.yml', 'data/icdc-model-props.yml']), ICDC_Schema)
+        schema = ICDC_Schema(['data/icdc-model.yml', 'data/icdc-model-props.yml'])
+        self.assertIsInstance(schema, ICDC_Schema)
+        self.assertEqual(29, schema.node_count())
+        self.assertEqual(37, schema.relationship_count())
+
 
 if __name__ == '__main__':
     unittest.main()
