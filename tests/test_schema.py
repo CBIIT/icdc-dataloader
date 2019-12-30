2816f8955d8a0d5f1361651997272b824a40f36b
Ming Ying <yingming18@gmail.com>
2019-12-30 11:35:14 -0500

    Update from latest bento-common changes.

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -11,7 +11,7 @@ class TestSchema(unittest.TestCase):
         self.assertRaises(Exception, ICDC_Schema, ['a', 'b'])
         schema = ICDC_Schema(['data/icdc-model.yml', 'data/icdc-model-props.yml'])
         self.assertIsInstance(schema, ICDC_Schema)
-        self.assertEqual(28, schema.node_count())
+        self.assertEqual(25, schema.node_count())
         self.assertEqual(43, schema.relationship_count())
 
     def test_default_value(self):
