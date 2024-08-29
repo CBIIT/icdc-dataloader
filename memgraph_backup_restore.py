7d9f794087803022293db42eb59088a7dd526688
Fuyuan Wang <60631776+wfy1997@users.noreply.github.com>
2024-08-29 14:48:54 -0400

    Update the memgraph backup function

--- memgraph_backup_restore.py
+++ memgraph_backup_restore.py
@@ -1,6 +1,8 @@
 import os
 import shutil
 import subprocess
+import sys
+from bento.common.utils import get_time_stamp
 
 def get_latest_file(src_folder):
     files = [os.path.join(src_folder, f) for f in os.listdir(src_folder) if os.path.isfile(os.path.join(src_folder, f))]
@@ -8,6 +10,25 @@ def get_latest_file(src_folder):
         return None
     latest_file = max(files, key=os.path.getmtime)
     return latest_file
+
+def backup_memgraph_mgconsole(memgraph_backup_dir, memgraph_dump_dir, memgraph_username, memgraph_password, log):
+    try:
+        timestamp = get_time_stamp()
+        memgraph_dump_file_name = "memgraph_dump" + "_" + timestamp + ".cypherl"
+        memgraph_dump_file_key = os.path.join("/"+os.path.basename(memgraph_dump_dir), memgraph_dump_file_name )
+        backup_command = f'''docker exec -it memgraph_docker-memgraph-1 bash -c \
+        'echo "DUMP DATABASE;" | mgconsole \
+        --username {memgraph_username} --password {memgraph_password} --output-format=cypherl > {memgraph_dump_file_key}' \
+        '''
+        subprocess.run(backup_command, shell=True)
+        dump_file_key = os.path.join(memgraph_dump_dir, memgraph_dump_file_name)
+        dest_file_path = os.path.join(memgraph_backup_dir, memgraph_dump_file_name)
+        shutil.copy2(dump_file_key, dest_file_path)
+        log.info(f"Successfully copied the backup snapshot file {dump_file_key} to {dest_file_path}")
+        return dest_file_path
+    except Exception as e:
+        log.error(e)
+        return "Backup Memgraph failed"
 
 def backup_memgraph(memgraph_backup_dir, memgraph_snapshot_dir, log):
     try:
