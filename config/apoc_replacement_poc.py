34ecc9af30a65704a8eedcabd0fb2ae6cbd398e4
adamdavenportGov <143554568+adamdaventryGov@users.noreply.github.com>
2025-02-11 10:42:18 -0500

    Added apoc replacement to the Dataloader
    Added apoc replacement to the Dataloader, This file is to be stored in the memgraph database internal_modules folder when you boot up your server so you can use the Apoc python replacements. These contain everything you need to load memgraph data to CTDC and Bento Opensearch/ElasticSearch databases.

---
+++ config/apoc_replacement_poc.py
@@ -0,0 +1,56 @@
+import mgp
+import json
+
+@mgp.function
+def merge(map_a: mgp.Map, map_b: mgp.Map) -> mgp.Map:
+    map_a.update(map_b)
+    return map_a
+
+
+@mgp.function
+def to_set(collection: list) -> list:
+    seen = set()  
+    result = []   
+    
+    for item in collection:
+
+        item_frozenset = frozenset(item.items())
+        
+
+        if item_frozenset not in seen:
+            seen.add(item_frozenset)
+            result.append(item)
+    
+    return result
+
+
+@mgp.function
+def sort(collection: list) -> list:
+    sorted_list = list(collection)
+    sorted_list.sort()
+    return sorted_list
+
+
+@mgp.function
+def flatten(collection: list) -> list:
+    new_list = []
+    for sub_collection in collection:
+        sub_collection = list(sub_collection)
+        for element in sub_collection:
+            new_list.append(element)
+    return new_list
+    
+@mgp.function
+def join_collection(collection: list, separator: str = '') -> str:
+    result = ''
+    for i, item in enumerate(collection):
+        if i == 0:
+            result += str(item)  
+        else:
+            result += separator + str(item)
+
+    return result
+@mgp.function
+def json_to_cypher(json_list):
+    cypher_list = "[" + ", ".join(map(str, json_list)) + "]"
+    return cypher_list
