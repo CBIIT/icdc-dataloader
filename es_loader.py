5bf25142cefd5ebbc82b9dedbbe8913f03fcbd59
Eric Miller <ermiller@alumni.cmu.edu>
2026-04-13 11:14:41 -0400

    fix: use recreate_index function when creating external data index

--- es_loader.py
+++ es_loader.py
@@ -273,7 +273,7 @@ def main():
                     f'"model_files" not set in configuration file, {index_name} will not be loaded!')
         elif index['type'] == 'external':
             logger.info("External data index created - loading will be done via data retriever service")
-            loader.create_index(index_name, index["mapping"])
+            loader.recreate_index(index_name, index["mapping"])
             summary[index_name] = "Index created"
         else:
             logger.error(f'Unknown index type: "{index["type"]}"')
